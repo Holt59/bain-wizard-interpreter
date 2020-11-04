83b782db14b9ee2f470f000427367d144b20f5db
Mikaël Capelle <capelle.mikael@gmail.com>
2020-11-04 21:14:11 +0100

    Add setup.py.

---
+++ setup.py
@@ -0,0 +1,42 @@
+# -*- encoding: utf-8 -*-
+
+from setuptools import setup
+
+dev_requires = [
+    "black",
+    "flake8",
+    "flake8-black",
+    "mypy",
+    "pytest",
+]
+
+setup(
+    # Name of the package:
+    name="bain-wizard-interpreter",
+    # Version of the package:
+    version="0.0.1",
+    # Find the package automatically (include everything):
+    packages=["wizard"],
+    # Author information:
+    author="Holt59",
+    author_email="capelle.mikael@gmail",
+    # Description of the package:
+    description="A BAIN Wizard Interpreter based on wizparse.",
+    long_description=open("README.md").read(),
+    # URL for sources:
+    url="https://github.com/Holt59/bain-wizard-interpreter",
+    classifiers=[
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+    ],
+    # License:
+    license="MIT",
+    # Requirements:
+    install_requires=[
+        "antlr4-python3-runtime",
+    ],
+    extras_require={"dev": dev_requires},
+)
