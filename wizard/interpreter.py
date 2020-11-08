636bd209fe25352c81b4f2eb34326fb6aa670db1
Mikaël Capelle <capelle.mikael@gmail.com>
2020-11-08 13:11:52 +0100

    Add WizardInterpreter.exec_until().

--- wizard/interpreter.py
+++ wizard/interpreter.py
@@ -1,13 +1,15 @@
 # -*- encoding: utf-8 -*-
 
 from pathlib import Path
-from typing import Optional, Union, TextIO, overload
+from typing import Any, Optional, Sequence, TextIO, Type, Union, overload
 
 from antlr4 import InputStream
 
 from .contexts import (
+    WizardInterpreterContext,
     WizardInterpreterContextFactory,
     WizardTopLevelContext,
+    WizardTerminationContext,
 )
 from .state import ContextState, WizardInterpreterState
 from .utils import make_top_level_context
@@ -31,6 +33,31 @@ class WizardInterpreter:
             factory: The context factory.
         """
         self._factory = factory
+
+    def exec_until(
+        self,
+        context: WizardInterpreterContext[ContextState, Any],
+        targets: Sequence[Type[WizardInterpreterContext]],
+    ) -> WizardInterpreterContext[ContextState, Any]:
+        """
+        Execute the contexts until a context of the given type is found, or
+        a termination context is found. If context is one of the given targets,
+        the function returns immediately.
+
+        Args:
+            context: The start context.
+            targets: Target context types. If one of those context is found,
+                the process stops.
+
+        Returns:
+            The next context (one of the targets or a termination contexts).
+        """
+        stargets = tuple(targets)
+
+        while not isinstance(context, stargets + (WizardTerminationContext,)):
+            context = context.exec()
+
+        return context
 
     @overload
     def make_top_level_context(
